6cd33b26a22ee4db1cb089305603937f4e17dd51
wangyanqing <625259292@qq.com>
2019-02-13 22:18:47 +0800

    modify

--- httpdownload.go
+++ httpdownload.go
@@ -3,15 +3,15 @@ package main
 import (
 	"flag"
 	"fmt"
-	. "github.com/gtechx/base/common"
-	"golang.org/x/net/proxy"
-	"io/ioutil"
 	"net/http"
 	"net/url"
 	"os"
 	"path/filepath"
 	"strings"
 	"time"
+
+	. "github.com/gtechx/base/common"
+	"golang.org/x/net/proxy"
 )
 
 var rawurl string = ""
@@ -116,9 +116,6 @@ func testhttp() {
 
 	startbyte := 0
 	endbyte := perbytes - 1
-	var endchan chan int
-	endchan = make(chan int, threadnum)
-	count := threadnum
 	starttime := time.Now().Unix()
 
 	purl, _ := url.Parse(rawurl)
@@ -131,71 +128,44 @@ func testhttp() {
 	f.Truncate(resp.ContentLength)
 	defer f.Close()
 
+	downloaders := make([]*Downloader, threadnum)
 	for i := 0; i < threadnum; i++ {
 		if i == threadnum-1 {
 			endbyte += remainbytes
 		}
-		go func(startbyte, endbyte, index int) {
-			lstart := startbyte
-			lend := endbyte
 
-			// setup a http client
-			httpTransport := &http.Transport{}
-			httpClient := &http.Client{Transport: httpTransport}
-			// set our socks5 as the dialer
-			if proxyaddr != "" {
-				var auth *proxy.Auth = nil
-				if proxyauth != "" {
-					autharr := strings.Split(proxyauth, ":")
-					if len(autharr) > 1 {
-						auth = &proxy.Auth{User: autharr[0], Password: autharr[1]}
-					}
-				}
-				var dialer proxy.Dialer
-				if bsock5 {
-					dialer, err = proxy.SOCKS5("tcp", proxyaddr, auth, proxy.Direct)
-				} else {
-					rurl, err := url.Parse(proxyaddr)
-					if err == nil {
-						dialer, err = proxy.FromURL(rurl, proxy.Direct)
-					}
-				}
-				if err != nil {
-					fmt.Fprintln(os.Stderr, "can't connect to the proxy:", err)
-					//os.Exit(1)
-				} else {
-					httpTransport.Dial = dialer.Dial
-				}
-			}
+		d := NewDownloader(outputdir, rawurl, startbyte, endbyte, i, proxyaddr != "", proxyaddr, proxyauth, bsock5)
+		downloaders[i] = d
+		go d.Start()
 
-			req, err := http.NewRequest("GET", rawurl, nil)
-			req.Header.Add("Range", `bytes=`+String(lstart)+`-`+String(lend))
-			fmt.Println(index, "Range", `bytes=`+String(lstart)+`-`+String(lend)+" start")
-			resp, err := httpClient.Do(req)
-			if err != nil {
-				fmt.Println("Range", `bytes=`+String(lstart)+`-`+String(lend)+" req error ", err.Error())
-			} else {
-				data, err := ioutil.ReadAll(resp.Body)
-				if err != nil {
-					fmt.Println("Range", `bytes=`+String(lstart)+`-`+String(lend)+" read error ", err.Error())
-				} else {
-					//fmt.Println("Range", `bytes=`+String(lstart)+`-`+String(lend)+" end:contentlength ", resp.ContentLength, " status:", resp.Status)
-					f.Seek(int64(startbyte), 0)
-					f.Write(data)
-				}
-				endchan <- 1
-			}
-		}(startbyte, endbyte, i)
 		startbyte += perbytes
 		endbyte += perbytes
 	}
 
-	for _ = range endchan {
-		count -= 1
-		if count == 0 {
+	for _, d := range downloaders {
+		<-d.Done
+	}
+
+	for i, d := range downloaders {
+		df, err := os.OpenFile(outputdir+fpath+String(d.Index), os.O_RDWR, os.ModePerm) //可读写，追加的方式打开（或创建文件）
+		if err != nil {
+			fmt.Println("open file index ", i, " error:", err.Error())
 			break
 		}
+		f.Seek(int64(d.StartByte), 0)
+
+		buff := make([]byte, 1024)
+
+		for {
+			c, err := df.Read(buff)
+			if err != nil || c == 0 {
+				break
+			}
+
+			f.Write(buff[:c])
+		}
 	}
+
 	usetime := time.Now().Unix() - starttime
 	speed := float64(resp.ContentLength) / float64(usetime) / 1024.0
 
@@ -210,4 +180,112 @@ func testhttp() {
 	// fmt.Println("download end, use", usetime, "seconds, speed:", String(Int64))+"KB/s")
 	// a := 0
 	// fmt.Scanln(&a)
+}
+
+type Downloader struct {
+	DownloadPath string
+	Url          string
+	StartByte    int
+	EndByte      int
+	Index        int
+	UseProxy     bool
+	Proxy        string
+	ProxyAuth    string
+	UseSock5     bool
+
+	Done chan bool
+}
+
+func NewDownloader(downloadpath, url string, startbyte, endbyte int, index int, useproxy bool, proxy, proxyauth string, usesock5 bool) *Downloader {
+	d := &Downloader{}
+	d.DownloadPath = downloadpath
+	d.Url = url
+	d.StartByte = startbyte
+	d.EndByte = endbyte
+	d.Index = index
+	d.UseProxy = useproxy
+	d.Proxy = proxy
+	d.ProxyAuth = proxyauth
+	d.UseSock5 = usesock5
+
+	d.Done = make(chan bool, 1)
+
+	return d
+}
+
+func (d *Downloader) Start() {
+	var err error
+
+	purl, _ := url.Parse(d.Url)
+	fname := filepath.Base(purl.Path)
+
+	httpTransport := &http.Transport{}
+	httpClient := &http.Client{Transport: httpTransport}
+
+	if d.UseProxy {
+		// set our socks5 as the dialer
+		if d.Proxy != "" {
+			var auth *proxy.Auth = nil
+			if d.ProxyAuth != "" {
+				autharr := strings.Split(d.ProxyAuth, ":")
+				if len(autharr) > 1 {
+					auth = &proxy.Auth{User: autharr[0], Password: autharr[1]}
+				}
+			}
+			var dialer proxy.Dialer
+			if d.UseSock5 {
+				dialer, err = proxy.SOCKS5("tcp", d.Proxy, auth, proxy.Direct)
+			} else {
+				rurl, err := url.Parse(d.Proxy)
+				if err == nil {
+					dialer, err = proxy.FromURL(rurl, proxy.Direct)
+				}
+			}
+			if err != nil {
+				fmt.Fprintln(os.Stderr, "can't connect to the proxy:", err)
+				//os.Exit(1)
+			} else {
+				httpTransport.Dial = dialer.Dial
+			}
+		}
+	}
+
+	req, err := http.NewRequest("GET", d.Url, nil)
+	req.Header.Add("Range", `bytes=`+String(d.StartByte)+`-`+String(d.EndByte))
+	fmt.Println(d.Index, "Range", `bytes=`+String(d.StartByte)+`-`+String(d.EndByte)+" start")
+	resp, err := httpClient.Do(req)
+
+	if err != nil {
+		fmt.Println("Range", `bytes=`+String(d.StartByte)+`-`+String(d.EndByte)+" req error ", err.Error())
+	} else {
+		f, err := os.OpenFile(d.DownloadPath+fname+String(d.Index), os.O_RDWR|os.O_CREATE, os.ModePerm) //可读写，追加的方式打开（或创建文件）
+		if err != nil {
+			fmt.Println("Range", `bytes=`+String(d.StartByte)+`-`+String(d.EndByte)+" create file error ", err.Error())
+			d.Done <- true
+			return
+		}
+		f.Truncate(resp.ContentLength)
+		defer f.Close()
+
+		buff := make([]byte, 1024000)
+
+		sum := 0
+
+		for {
+			count, err := resp.Body.Read(buff)
+
+			if err != nil {
+				fmt.Println("Range", `bytes=`+String(d.StartByte)+`-`+String(d.EndByte)+" read error ", err.Error())
+				break
+			} else if count == 0 {
+				fmt.Println("Range", `bytes=`+String(d.StartByte)+`-`+String(d.EndByte)+" read end ")
+			} else {
+				sum += count
+				//per := fmt.Sprintf("%.2f", float64(sum)/float64(d.EndByte-d.StartByte))
+				//fmt.Println("Range index ", d.Index, ` bytes=`+String(d.StartByte)+`-`+String(d.EndByte)+" download percent: ", per, "% "+String(sum)+"/"+String(d.EndByte-d.StartByte), " count ", count)
+				f.Write(buff[:count])
+			}
+		}
+	}
+	d.Done <- true
 }
